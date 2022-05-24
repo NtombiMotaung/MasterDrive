dff72126e4ee5cb4609e13c2037932b1f8cc23f5
nmotaung021 <nmotaung021@student.wethinkcode.co.za>
2022-05-24 14:09:39 +0200

    added images

--- mysite/services/urls.py
+++ mysite/services/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-from services.views import get_services
+from services.views import get_services, get_service
 
 
 app_name = "services"
 
-urlpatterns = [path("", get_services, name="get-services-page")]
+urlpatterns = [
+
+    path("", get_services, name="get-services-page"),
+    path("<id>/", get_service, name="get-service"),
+
+
+]
--- mysite/services/views.py
+++ mysite/services/views.py
@@ -1,6 +1,21 @@
+from django.http import Http404
 from django.shortcuts import render
-
+from services.models import Service
 
 # Create your views here.
 def get_services(request):
     return render(request, "public/services/services.html")
+
+def get_service(request, id):
+    
+    try:
+        services = Service.objects.get(id = id)
+
+    except:
+        raise Http404("Service not found")
+
+    context = {"request" : request, "services" : services} 
+
+
+    return render(request, "public/services/get-services.html", context)
+
