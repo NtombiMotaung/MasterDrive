0466e7b6fdf9906047167097aa1e8d542bb3856d
nmotaung021 <nmotaung021@student.wethinkcode.co.za>
2022-05-24 13:31:31 +0200

    added some htmls

---
+++ mysite/services/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from services.views import get_services
+
+
+
+app_name = "services"
+
+urlpatterns = [
+    path("", get_services, name = "get-services-page")
+]
--- mysite/services/views.py
+++ mysite/services/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def get_services(request):
+    return render(request, "public/services/services.html")
