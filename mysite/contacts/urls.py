ad71795466dc2f2b431a7e45525ac2b94dd7a77e
nmotaung021 <nmotaung021@student.wethinkcode.co.za>
2022-05-29 16:33:06 +0200

    done with contacts just html body needed

---
+++ mysite/contacts/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from contacts.views import contacts
+
+
+app_name = "contacts"
+
+urlpatterns = [
+
+    path("", contacts, name="get-contacts-page"),
+    
+
+
+]
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -7,6 +7,7 @@ urlpatterns = (
     [
         path("staff/", admin.site.urls),
         path("services/", include("services.urls", namespace="services")),
+        path("contacts/", include("contacts.urls", namespace = "contacts")),
     ]
     + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
