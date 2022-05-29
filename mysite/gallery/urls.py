ad0b0b836d491e456da46251a0399b81c943bcd1
nmotaung021 <nmotaung021@student.wethinkcode.co.za>
2022-05-29 16:58:38 +0200

    done with gallery just need to add html body

---
+++ mysite/gallery/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from gallery.views import gallery
+
+
+app_name = "gallery"
+
+urlpatterns = [
+
+    path("", gallery, name="get-gallery-page"),
+    
+
+
+]
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -8,6 +8,7 @@ urlpatterns = (
         path("staff/", admin.site.urls),
         path("services/", include("services.urls", namespace="services")),
         path("contacts/", include("contacts.urls", namespace = "contacts")),
+        path("gallery/", include("gallery.urls", namespace= "gallery") )
     ]
     + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
