55955ea3f6493e693e8dcd430192c24f692eaa99
nmotaung021 <nmotaung021@student.wethinkcode.co.za>
2022-05-29 17:56:25 +0200

    changes

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -2,13 +2,18 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import render
+
+def index(request):
+    return render(request, "public/index.html")
 
 urlpatterns = (
     [
         path("staff/", admin.site.urls),
         path("services/", include("services.urls", namespace="services")),
         path("contacts/", include("contacts.urls", namespace = "contacts")),
-        path("gallery/", include("gallery.urls", namespace= "gallery") )
+        path("gallery/", include("gallery.urls", namespace= "gallery") ),
+        path("",  index),
     ]
     + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
