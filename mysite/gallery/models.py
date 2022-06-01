cac69222dfc78722cacc8c035573ddb124cc7689
nmotaung021 <nmotaung021@student.wethinkcode.co.za>
2022-06-01 16:28:22 +0200

    added to gallery images

--- mysite/gallery/models.py
+++ mysite/gallery/models.py
@@ -12,10 +12,28 @@ class GalleryImages(models.Model):
     upload_to="posts/bg/",
     )
     image2 = models.ImageField(
-        upload_to="posts/bg/",
+    upload_to="posts/bg/",
     )
     image3 = models.ImageField(
-        upload_to="posts/bg/",
+    upload_to="posts/bg/",
+    )
+    image4 = models.ImageField(
+    upload_to="posts/bg/",
+    )
+    image5 = models.ImageField(
+    upload_to="posts/bg/",
+    )
+    image6 = models.ImageField(
+    upload_to="posts/bg/",
+    )
+    image7 = models.ImageField(
+    upload_to="posts/bg/",
+    )
+    image8 = models.ImageField(
+    upload_to="posts/bg/",
+    )
+    image9 = models.ImageField(
+    upload_to="posts/bg/",
     )
 
 
