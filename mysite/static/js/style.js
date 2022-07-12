fa610520ec0e96bf564ec29dd3efb4eb5b3fc6af
nmotaung021 <nmotaung021@student.wethinkcode.co.za>
2022-07-12 10:02:48 +0200

    phone validation on contacts form now working

--- mysite/static/js/style.js
+++ mysite/static/js/style.js
@@ -1,24 +1,15 @@
-// Get the modal
-var modal = document.getElementById("myModal");
+const nameInput = document.querySelector("#name");
+const email = document.querySelector("#name");
+const message = document.querySelector("#name");
+const success = document.querySelector("#name");
+const errorm = document.querySelector("#name");
 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("myImg");
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
+//validating the data
+
+function validateForm() {
+  if (nameInput.ariaValueMax.length < 1) {
+    errorm[0].innerText = "Name cannot be blank";
+    nameInput.classList.add("error-border");
+  }
+  
 }
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-} 
-
-function myFunction(x) {
-  x.classList.toggle("change");
-} 
